51efc25d9793f3a2815905b5abbe78d659188d88
raonismaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-09-30 17:36:55 -0300

    fixing run.sh

--- run.sh
+++ run.sh
@@ -9,39 +9,6 @@ if [ -z "$DIR_PATH" ] ; then
   exit 1  # fail
 fi
 
-# docker setup
-install-docker() {
-    echo "--> Installing docker"
-    apt update
-
-    sudo apt-get install -y \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        software-properties-common
-    
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-    sudo apt-key fingerprint 0EBFCD88
-
-    sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
-
-    sudo apt-get update
-
-    sudo apt-get install -y docker-ce
-}
-
-CHECK_DOCKER_INSTALLATION=$(dpkg -l | grep -c docker-ce)
-
-if ! [ $CHECK_DOCKER_INSTALLATION -ne 0 ]; then
-    install-docker
-else 
-    echo "--> Docker its already installed"
-fi
-
 image_tag=${1-latest}
 image=eubraatmosphere/fogbow-probes:$image_tag
 conf_file=$DIR_PATH/probe-fogbow.conf
